0b86e8d543bf324c9d60abd09158424a8ac51ae2
zofia janas <zoska91@wp.pl>
2024-04-16 18:24:50 +0200

    add eas config + change nam for eas + change auth loader

---
+++ readme.md
@@ -0,0 +1,1 @@
+1. add redirect to auth page
--- src/screens/HomeScreen/HomeScreen.tsx
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -4,8 +4,10 @@ import { TitleWrapper, TitleText, Auth } from '../../components';
 import Layout from '../../layout';
 
 import * as S from './HomeScreen.css';
+import { useGlobalProvider } from '../../layout/GlobalProvider';
 
 const HomeScreen: FC = () => {
+  const { isLoading } = useGlobalProvider();
   return (
     <S.Wrapper>
       <Layout>
@@ -14,7 +16,7 @@ const HomeScreen: FC = () => {
           <TitleText>one Word</TitleText>
           <TitleText small>a day</TitleText>
         </TitleWrapper>
-        <Auth />
+        {!isLoading && <Auth />}
       </Layout>
     </S.Wrapper>
   );
